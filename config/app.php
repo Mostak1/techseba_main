06c8c04d0fb4ad56eb32b8d34f8e655109b869ce
Mostak1 <mostakidb@gmail.com>
2026-05-11 12:00:54 +0600

    Manually register DomPDF provider and alias

--- config/app.php
+++ config/app.php
@@ -187,6 +187,7 @@ return [
          */
 
          Intervention\Image\ImageServiceProvider::class,
+         Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -212,7 +213,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Pdf' => Barryvdh\DomPDF\Facade\Pdf::class,
     ])->toArray(),
 
 ];

978af5ec3d56490096f3383f130ceee6371dc4b9
Mostak1 <mostakidb@gmail.com>
2026-06-02 14:30:24 +0600

    feat: integrate Google Tag Manager tracking scripts into head and layout views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,10 @@
 <html lang="en">
     @include('frontend.head')
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGH5WHH2"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <!-- Menu Start -->
     <div class="optech-preloader-wrap">
